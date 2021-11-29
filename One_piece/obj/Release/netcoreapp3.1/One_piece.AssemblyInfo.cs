c143a49c6d7a7d2b152dcd1d142cde66c6c212d7
shan <s107676@ap.be>
2021-11-29 10:52:35 +0100

    shan comm

--- One_piece/obj/Release/netcoreapp3.1/One_piece.AssemblyInfo.cs
+++ One_piece/obj/Release/netcoreapp3.1/One_piece.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("One_piece")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("One_piece")]
